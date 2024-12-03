3607042bab6af021a8ff1e0d9bc59d20cecde14c
Michael McIntyre <mpmcintyre@yahoo.com>
2024-12-03 21:29:19 +0100

    Adding json and yaml file types

--- internal/config.go
+++ internal/config.go
@@ -1,11 +1,14 @@
 package runner
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/BurntSushi/toml"
 	color "github.com/fatih/color"
+	"gopkg.in/yaml.v3"
 )
 
 type (
@@ -14,22 +17,21 @@ type (
 	ExitStatus  int
 
 	Process struct {
-		Name             string      `toml:"name"`
-		Command          string      `toml:"command"`
-		Args             []string    `toml:"args"`
-		Prefix           string      `toml:"prefix"`
-		Color            ColourCode  `toml:"color"`
-		OnFailure        ExitCommand `toml:"on_failure"`
-		OnComplete       ExitCommand `toml:"on_complete,omitempty"`
-		SeperateNewLines bool
+		Name             string      `toml:"name" json:"name" yaml:"name"`
+		Command          string      `toml:"command" json:"command" yaml:"command"`
+		Args             []string    `toml:"args" json:"args" yaml:"args"`
+		Prefix           string      `toml:"prefix" json:"prefix" yaml:"prefix"`
+		Color            ColourCode  `toml:"color" json:"color" yaml:"color"`
+		OnFailure        ExitCommand `toml:"on_failure" json:"on_failure" yaml:"on_failure"`
+		OnComplete       ExitCommand `toml:"on_complete,omitempty" json:"on_complete,omitempty" yaml:"on_complete,omitempty"`
+		SeperateNewLines bool        `json:"seperate_new_lines" yaml:"seperate_new_lines"`
 		ShowTimestamp    bool
 		Status           ExitStatus
 	}
-
 	Config struct {
-		Processes        []Process `toml:"processes"`
-		SeperateNewLines bool      `toml:"indicate_every_line"`
-		ShowTimestamp    bool      `toml:"show_timestamp"`
+		Processes        []Process `toml:"processes" json:"processes" yaml:"processes"`
+		SeperateNewLines bool      `toml:"indicate_every_line" json:"indicate_every_line" yaml:"indicate_every_line"`
+		ShowTimestamp    bool      `toml:"show_timestamp" json:"show_timestamp" yaml:"show_timestamp"`
 		filePresent      bool
 	}
 )
@@ -93,17 +95,52 @@ func (c *Config) ParseFile(path string) error {
 	}
 	text := string(buffer)
 
-	_, err2 := toml.Decode(text, &c)
-	if err2 != nil {
-		return err2
+	extensions := strings.Split(path, ".")
+	switch extensions[len(extensions)] {
+	case "toml":
+
+		_, err := toml.Decode(text, &c)
+		if err != nil {
+			return err
+		}
+
+		fmt.Printf("Found %d processes in %s\n", len(c.Processes), path)
+		for i := range c.Processes {
+			fmt.Printf("%#v\n", c.Processes[i].Name)
+			c.Processes[i].SeperateNewLines = c.SeperateNewLines
+			c.Processes[i].ShowTimestamp = c.ShowTimestamp
+		}
+		c.filePresent = true
+
+	case "json":
+		jsonData, err := json.Marshal(text)
+		if err != nil {
+			return err
+		}
+		err = json.Unmarshal(jsonData, &c)
+		if err != nil {
+			return err
+		}
+
+	case "yaml":
+		yamlData, err := yaml.Marshal(text)
+		if err != nil {
+			return err
+		}
+		err = yaml.Unmarshal(yamlData, &c)
+		if err != nil {
+			return err
+		}
+	case "yml":
+		yamlData, err := yaml.Marshal(text)
+		if err != nil {
+			return err
+		}
+		err = yaml.Unmarshal(yamlData, &c)
+		if err != nil {
+			return err
+		}
 	}
 
-	fmt.Printf("Found %d processes in %s\n", len(c.Processes), path)
-	for i := range c.Processes {
-		fmt.Printf("%#v\n", c.Processes[i].Name)
-		c.Processes[i].SeperateNewLines = c.SeperateNewLines
-		c.Processes[i].ShowTimestamp = c.ShowTimestamp
-	}
-	c.filePresent = true
 	return nil
 }
