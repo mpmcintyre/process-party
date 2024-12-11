42b056bb7c6f522425aa1c8ee19ebdfc0bd3b350
Michael McIntyre <mpmcintyre@yahoo.com>
2024-12-11 20:49:26 +0100

    Added test helpers mod

---
+++ test_helpers/test_helpers.go
@@ -0,0 +1,30 @@
+package testHelpers
+
+import "fmt"
+
+type CmdSettings struct {
+	Cmd  string
+	Args []string
+}
+
+// Exported functions
+func CreateSleepCmdSettings(sleepDurationSeconds int) CmdSettings {
+	return CmdSettings{
+		Cmd:  "go",
+		Args: []string{"run", "./mocks/fake_process.go", "sleep", fmt.Sprintf("%d", sleepDurationSeconds)},
+	}
+}
+
+func CreateTouchCmdSettings(filename string) CmdSettings {
+	return CmdSettings{
+		Cmd:  "go",
+		Args: []string{"run", "./mocks/fake_process.go", "touch", filename},
+	}
+}
+
+func CreateMkdirCmdSettings(dirname string) CmdSettings {
+	return CmdSettings{
+		Cmd:  "go",
+		Args: []string{"run", "./mocks/fake_process.go", "mkdir", dirname},
+	}
+}
